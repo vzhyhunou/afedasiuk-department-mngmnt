ef68048b6154e4d7331f9291d69355c31ebc292f
Aliaksandr Fedasiuk <Aliaksandr_Fedasiuk@epam.com>
2020-02-13 18:03:16 +0300

    Department Managemet model and dao 1st init

---
+++ department-management-dao/src/main/java/com/epam/brest/courses/dao/DepartmentDao.java
@@ -0,0 +1,11 @@
+package com.epam.brest.courses.dao;
+
+import com.epam.brest.courses.model.Department;
+
+import java.util.List;
+
+public interface DepartmentDao {
+
+    List<Department> getDepartments();
+
+}
