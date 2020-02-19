b338fe511b1713d09bb2e7eca0739fada311f7b5
Aliaksandr Fedasiuk <Aliaksandr_Fedasiuk@epam.com>
2020-02-19 21:09:44 +0300

    add logging

--- department-management-dao/src/main/java/com/epam/brest/courses/dao/DepartmentJdbcDaoImpl.java
+++ department-management-dao/src/main/java/com/epam/brest/courses/dao/DepartmentJdbcDaoImpl.java
@@ -1,6 +1,8 @@
 package com.epam.brest.courses.dao;
 
 import com.epam.brest.courses.model.Department;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
@@ -10,6 +12,8 @@ import java.util.List;
 
 public class DepartmentJdbcDaoImpl implements DepartmentDao {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(DepartmentJdbcDaoImpl.class);
+
     private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 
     public DepartmentJdbcDaoImpl(NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
@@ -18,6 +22,7 @@ public class DepartmentJdbcDaoImpl implements DepartmentDao {
 
     @Override
     public List<Department> getDepartments() {
+        LOGGER.trace("Get all departments {}", 0);
         List<Department> departments = namedParameterJdbcTemplate
                 .query("SELECT d.departmentId, d.departmentName FROM department d ORDER BY d.departmentName", new DepartmentRowMapper());
         return departments;
