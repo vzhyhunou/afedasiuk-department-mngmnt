e799a5f634ab080ec6454a5ef61f31d0c498d71c
berdachuk <berdachuk@berdaflex.com>
2020-03-04 12:10:01 +0300

    add model to departments controller

--- department-management-web-app/src/main/java/com/epam/brest/courses/web_app/DepartmentController.java
+++ department-management-web-app/src/main/java/com/epam/brest/courses/web_app/DepartmentController.java
@@ -1,5 +1,8 @@
 package com.epam.brest.courses.web_app;
 
+import com.epam.brest.courses.service.DepartmentDtoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +13,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class DepartmentController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(DepartmentController.class);
+
+    private final DepartmentDtoService departmentDtoService;
+
+    public DepartmentController(DepartmentDtoService departmentDtoService) {
+        this.departmentDtoService = departmentDtoService;
+    }
+
     /**
      * Goto departments list page.
      *
@@ -17,6 +28,9 @@ public class DepartmentController {
      */
     @GetMapping(value = "/departments")
     public final String departments(Model model) {
+
+        LOGGER.debug("departments()");
+        model.addAttribute("departments", departmentDtoService.findAllWithAvgSalary());
         return "departments";
     }
 
